d3df12148ac6ad8c8d7cef998a38b36a03e50f22
dahezhiquan <3390205563@qq.com>
2024-01-10 15:29:22 +0800

    feat: 新增model层

--- pkg/dao/blog.go
+++ pkg/dao/blog.go
@@ -5,7 +5,6 @@ import (
 	"m-sec/pkg/connections/database"
 	"m-sec/pkg/connections/database/gorms"
 	"m-sec/pkg/model"
-	"m-sec/pkg/public"
 )
 
 type BlogDao struct {
@@ -19,24 +18,9 @@ func NewBlogDao() *BlogDao {
 func (b *BlogDao) FindBlogByHash(tx database.DbConn, ctx context.Context, hash string) (*model.Blog, error) {
 	var blog *model.Blog
 	conn := b.getConn(tx)
-	err := conn.Session(ctx).Where("hash = ? and status=1 and deleted = 0", hash).First(&blog).Error
+	err := conn.Session(ctx).Where("hash = ? and deleted = 0", hash).First(&blog).Error
 	if err == gorms.ErrRecordNotFound {
 		return nil, nil
 	}
 	return blog, err
-}
-
-func (b *BlogDao) FindBlogList(tx database.DbConn, ctx context.Context, page int) (list []*model.Blog, total int64, err error) {
-	conn := b.getConn(tx)
-	db := conn.Session(ctx).Model(&model.Blog{})
-	err = db.Scopes(gorms.Paginate(page, public.VUL_PAGE_SIZE)).Where("status=1 and deleted =0").Order("is_top DESC, id DESC").Find(&list).Error
-	if err != nil {
-		return nil, 0, err
-	}
-	err = db.Count(&total).Error
-	if err != nil {
-		return nil, 0, err
-	}
-
-	return list, total, nil
 }
